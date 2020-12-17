8c679479438cbfcad1551e97970b4980b7a85383
flexplaypruebas <75388413+flexplaypruebas@users.noreply.github.com>
2020-12-17 01:52:11 -0400

    Add files via upload

---
+++ php/phps/online.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Online</title>
+    <link rel="stylesheet" href="../css/estilos.css">
+</head>
+<body>
+    <?php  include('../phps/header.html')   ?>
+    <br><br>
+    <center>
+        <h1 style="font-size:50px;">Sub Pagina!!</h1>
+    </center>
+</body>
+</html>
